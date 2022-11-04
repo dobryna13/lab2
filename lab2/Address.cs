51530beb5c08518eb1fa95c0b2ff43e6c3440fc1
dobryna13 <mdobrinchuk@gmail.com>
2022-11-04 20:14:55 +0200

    add Address

---
+++ lab2/Address.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Address
+    {
+        private string index;
+        public string Index { get=>index; set=>index=value; }
+        private  string country;
+
+        public  string Country 
+        {
+            get { return country; }
+            set { country = value; }
+        }
+        private string city;
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        private string street;
+
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        private string house;
+
+        public string House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+        private int apartment;
+
+        public int Apartment
+        {
+            get { return apartment; }
+            set { apartment = value; }
+        }
+        public override string ToString()
+        {
+            return $"{Index}, {Country}, {City}, {Street}, {House}, {Apartment}";
+        }
+
+
+
+
+
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address {Index="1", Country="Ukraine", City="Kiev", Street="Lobachewskogo", House="23", Apartment=423};
+            Console.WriteLine(address.ToString());
+        }
+    }
+}
