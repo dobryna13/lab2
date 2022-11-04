72a1cd90c563e34d9018146b5eea2ce1963a1ef0
dobryna13 <mdobrinchuk@gmail.com>
2022-11-04 20:34:21 +0200

    Converter

---
+++ Converter/Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    public class Converter
+    {
+        private double usd;
+        private double eur;
+        private double rub;
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+        public double ConvertUsd(double sum)=>sum * usd;
+        public double ConvertEur(double sum)=>sum * eur;
+        public double ConvertRub(double sum)=>sum * rub;
+        public double ConvertToUsd(double sum)=>sum / usd;
+        public double ConvertToEur(double sum)=>sum / eur;
+        public double ConvertToRub(double sum)=>sum / rub;
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(36.57, 40, 0.3);
+            Console.WriteLine(converter.ConvertToEur(4000));
+            Console.WriteLine(converter.ConvertToUsd(4000));
+            Console.WriteLine(converter.ConvertToRub(4000));
+            Console.WriteLine(converter.ConvertEur(4000));
+            Console.WriteLine(converter.ConvertUsd(4000));
+            Console.WriteLine(converter.ConvertRub(4000));
+        }
+    }
+}
