f3b13969224dbc9f2b2cd96e0312c509d0e7404d
dobryna13 <mdobrinchuk@gmail.com>
2022-11-04 20:44:45 +0200

    Employee

---
+++ Employee/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+    {
+        public enum Position
+        {
+            Cleaner = 1,
+            Trainee,
+            Junior,
+            Middle,
+            Senior,
+            TeamLead,
+            Manager
+        }
+
+        public class Employee
+        {
+            private string name;
+            private string surname;
+            private double workPeriod;
+            private Position position;
+            private const int baseSalary = 4900;
+
+            public Employee(string name, string surname, double workPeriod, Position position)
+            {
+                Name = name;
+                Surname = surname;
+                this.workPeriod = workPeriod;
+                this.position = position;
+            }
+
+            public string Name { get => name; set => name = value; }
+            public string Surname { get => surname; set => surname = value; }
+
+            public double Salary => baseSalary * (int)position * (workPeriod / 1.5 + 1);
+            public double Tax => baseSalary * (int)position * 0.15;
+
+        }
+    }
---
+++ Employee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Maksym", "Dobrynchuk", 1, Position.Middle);
+            Console.WriteLine(employee.Salary.ToString());
+            Console.WriteLine(employee.Tax.ToString());
+        }
+    }
+}
